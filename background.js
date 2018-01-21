5d117720aed86fe98c8c7bb5d0847485227334e8
Kim Hamper <khamper+netjets@base2.io>
2018-01-21 11:22:51 -0500

    Display page action only when viewing GitHub issues

--- background.js
+++ background.js
@@ -1,18 +1,30 @@
 var branchName = null;
+var urlRegex = new RegExp(
+  "https://(www.)?github.com/(.+)/issues/([0-9]+)",
+  "i"
+);
+
+function isGitHubIssueUrl(url) {
+  return urlRegex.test(url);
+}
 
 function updateBranchName(tabId) {
-  chrome.pageAction.show(tabId); // TODO: Should not show all the time
   chrome.tabs.sendMessage(tabId, {}, name => {
     branchName = name;
   });
 }
 
-chrome.tabs.onUpdated.addListener(function(tabId, change) {
+chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
+  if (isGitHubIssueUrl(tab.url)) chrome.pageAction.show(tabId);
+  else chrome.pageAction.hide(tabId);
+
   if (change.status === "complete") {
     updateBranchName(tabId);
   }
 });
 
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  updateBranchName(tabs[0].id);
+  var currentTab = tabs[0];
+
+  updateBranchName(currentTab.id);
 });

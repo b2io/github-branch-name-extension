4030624ba83b653c07a2d4d589483ed2eb98c820
Kim Hamper <khamper+netjets@base2.io>
2018-01-21 11:23:20 -0500

    Copy to clipboard only if text is not empty

--- popup.js
+++ popup.js
@@ -1,3 +1,21 @@
+function isEmpty(text) {
+  return text === undefined || text === null || text.trim().length === 0;
+}
+
+function copyToClipboard(text) {
+  if (isEmpty(text)) return;
+
+  function handleCopy(event) {
+    document.removeEventListener("copy", handleCopy, true);
+    event.stopImmediatePropagation(); // prevent tampering
+    event.preventDefault();
+    event.clipboardData.setData("text/plain", text);
+  }
+
+  document.addEventListener("copy", handleCopy, true);
+  document.execCommand("copy");
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   var button = document.getElementById("generate-button");
 
